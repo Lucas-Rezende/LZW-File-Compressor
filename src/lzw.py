2540b55e4f5c160c3f1f1a3367d547418a348d56
Lucas Rezende <66080424+Lucas-Rezende@users.noreply.github.com>
2024-11-20 22:02:12 -0300

    fix

--- src/lzw.py
+++ src/lzw.py
@@ -95,18 +95,16 @@ class LZW:
         print(f" - Tempo total de execução: {self.stats['total_time']:.4f} segundos")
 
 def LZW_not_fixed_compress(input_file_path, max_bits=12):
-    # Gerar o nome do arquivo comprimido automaticamente com base no arquivo de entrada
     base_name = os.path.basename(input_file_path)
     compressed_file_path = os.path.splitext(base_name)[0] + '.lzw'
 
     with open(input_file_path, 'rb') as input_file, open(compressed_file_path, 'wb') as output_file:
-        # Inicializa o dicionário
         dicionario = CompactTrie2()
         for i in range(256):
-            dicionario[str(bytes([i]))] = i # 256 valores iniciais
+            dicionario[str(bytes([i]))] = i
         prefixo = bytes()
         current_bits = 9
-        dicionario_limited = False  # Flag para controlar o limite do dicionário
+        dicionario_limited = False
         dic_size = 256
         
         buffer = 0
@@ -114,42 +112,34 @@ def LZW_not_fixed_compress(input_file_path, max_bits=12):
 
         while True:
             byte = input_file.read(1)
-            if not byte:  # Se o arquivo foi totalmente lido
+            if not byte:
                 break
 
-            byte = byte[0]  # Acessa o valor do byte
+            byte = byte[0]
             if dicionario[str(prefixo + bytes([byte]))] != None:
                 prefixo += bytes([byte])
             else:
-                # Adiciona o código do prefixo à lista de códigos
                 code = dicionario[str(prefixo)]
-                #print(code)
                 buffer |= (code << bits_in_buffer)
                 bits_in_buffer += current_bits
 
-                # Quando o buffer atingir 8 bits ou mais, escreva o byte no fluxo
                 while bits_in_buffer >= 8:
                     output_file.write(bytes([buffer & 0xFF]))
                     buffer >>= 8
                     bits_in_buffer -= 8
 
-                # Se o dicionário não atingiu o limite, adiciona a nova sequência
                 if not dicionario_limited:
                     dicionario[str(prefixo + bytes([byte]))] = dic_size
                     dic_size += 1
 
-                # Atualiza o número de bits
                 if dic_size >= ((1 << current_bits) - 1):
                     if current_bits < max_bits:
                         current_bits += 1
                     else:
                         dicionario_limited = True
             
-
-                # O prefixo se torna o byte atual
                 prefixo = bytes([byte])
 
-        # Adiciona o último prefixo ao fluxo de saída
         if prefixo:
             code = dicionario[str(prefixo)]
             buffer |= (code << bits_in_buffer)
@@ -160,30 +150,25 @@ def LZW_not_fixed_compress(input_file_path, max_bits=12):
                 buffer >>= 8
                 bits_in_buffer -= 8
 
-        # Escreve o número máximo de bits (max_bits) no início do arquivo comprimido
         output_file.write(bytes([max_bits]))
 
     print(f"Arquivo comprimido gerado: {compressed_file_path}")
-
-
 
 def decompress_file_not_fixed(input_file_path, output_file_path):
     # Lê o arquivo comprimido
     with open(input_file_path, 'rb') as input_file, open(output_file_path, 'wb') as output_file:
         compressed_data = input_file.read()
-        max_bits = compressed_data[-1]  # O último byte é o max_bits
-        size_atual = 9  # Começa com 9 bits
+        max_bits = compressed_data[-1]
+        size_atual = 9
         possiveis = ((1 << size_atual) - 1) - 256
         lidos = 0
 
-        # Descartando o último byte que contém max_bits
         bit_stream = io.BytesIO(compressed_data[:-1])
 
         buffer = 0
         bits_in_buffer = 0
         compressed_codes = []
 
-        # Dicionário reverso inicial (código de 0-255)
         reverse_dicionario = {i: bytes([i]) for i in range(256)}
 
         while True:
@@ -195,38 +180,31 @@ def decompress_file_not_fixed(input_file_path, output_file_path):
             bits_in_buffer += 8
 
             while bits_in_buffer >= size_atual:
-                # Extrai o código do buffer
                 code = buffer & ((1 << size_atual) - 1)
                 compressed_codes.append(code)
                 lidos += 1
                 buffer >>= size_atual
                 bits_in_buffer -= size_atual
 
-                # Verifica se o dicionário precisa ser expandido
                 if lidos == possiveis:
                     if size_atual < max_bits:
                         size_atual += 1
                         possiveis = ((1 << size_atual) - 1) - 256
 
-        # Inicializa o prefixo com o primeiro código
-        prefixo = reverse_dicionario[compressed_codes.pop(0)]  # Remove o primeiro código da lista
-        output_file.write(prefixo)  # Escreve o primeiro prefixo
+        prefixo = reverse_dicionario[compressed_codes.pop(0)]
+        output_file.write(prefixo)
 
         for codigo in compressed_codes:
             if codigo in reverse_dicionario:
                 entry = reverse_dicionario[codigo]
             else:
-                # Se o código não estiver no dicionário, é uma sequência especial
                 entry = prefixo + prefixo[:1]
 
-            # Escreve a sequência no arquivo de saída
             output_file.write(entry)
 
-            # Adiciona uma nova entrada ao dicionário, se ainda não atingiu o limite
             if len(reverse_dicionario) <= ((1 << max_bits) - 1):
                 reverse_dicionario[len(reverse_dicionario)] = prefixo + entry[:1]
 
-            # Atualiza o prefixo para a última sequência
             prefixo = entry
 
     print(f"Arquivo descomprimido gerado: {output_file_path}")
